db44402e5f9b748c7e732700352fd692aaa4628f
vienna <stardog727@gmail.com>
2025-09-17 14:07:07 -0400

    Add LemonLight opmode and update Gradle plugin version
    Introduces LemonLight.java, an OpMode for interacting with the Limelight3A vision system, including initialization and pipeline switching. Also updates the Android Gradle plugin version in build.gradle from 8.7.0 to 8.5.1.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LemonLight.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class LemonLight extends OpMode {
+    Limelight3A limelight;
+
+    @Override
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
+        limelight.start(); // This tells Limelight to start looking!
+    }
+
+    limelight.pipelineSwitch(0); // Switch to pipeline number 0
+
+}
