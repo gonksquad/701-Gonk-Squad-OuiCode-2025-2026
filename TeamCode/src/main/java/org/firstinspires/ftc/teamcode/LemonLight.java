3412ff4ef46ca9e528e465a473142313b5f9521f
vienna <stardog727@gmail.com>
2025-09-17 15:32:27 -0400

    Refactor LemonLight to use LinearOpMode/ add base to lemonlight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LemonLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LemonLight.java
@@ -1,20 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.LLResultTypes;
-import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-public class LemonLight extends OpMode {
-    Limelight3A limelight;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+
+public class LemonLight extends LinearOpMode {
+
+    private Limelight3A limelight;
 
     @Override
-    public void init() {
+    //throws interrupted exception excluded
+    public void runOpMode() {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
-        limelight.start(); // This tells Limelight to start looking!
+
+        telemetry.setMsTransmissionInterval(11); //very frequent updates - every 11 ms
+
+        limelight.pipelineSwitch(0); //only pipeline we have now
+
+
+        limelight.start(); //starts polling for data, needed to get results
+
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+            if (result != null) {
+                if (result.isValid()) {
+                    Pose3D botpose = result.getBotpose();
+                    telemetry.addData("tx", result.getTx());
+                    telemetry.addData("ty", result.getTy());
+                    telemetry.addData("Botpose", botpose.toString());
+                }
+            }
+        }
     }
-
-    limelight.pipelineSwitch(0); // Switch to pipeline number 0
-
 }
