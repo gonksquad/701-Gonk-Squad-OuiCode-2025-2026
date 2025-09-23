32c5b56be3d7433864141518aa6faff3582e6e67
danny-reilly <daniel_reilly@students.nksd.net>
2025-09-23 14:41:42 -0400

    Code under Orangelight now detects the correct motif

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HELPMEcolorsensortesting.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.hardware.ColorSensor;
+
+@TeleOp (name="colorSensing")
+public class HELPMEcolorsensortesting extends LinearOpMode {
+
+    //private ColorSensor colorSensor;
+
+    @Override
+    public void runOpMode() {
+        /*colorSensor = hardwareMap.get(ColorSensor.class, "color_sensor");
+        colorSensor.enableLed(true); //on automatically i think but still
+        waitForStart();
+        while (opModeIsActive()) {
+            telemetry.addData("Red: ", colorSensor.red());
+            telemetry.addData("Green: ", colorSensor.green());
+            telemetry.addData("Blue: ", colorSensor.blue());
+           telemetry.update();
+        }*/
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OrangeLight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OrangeLight.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.List;
 
 //import edu.wpi.first.networktables.NetworkTable;
 //import edu.wpi.first.networktables.NetworkTableInstance;
@@ -16,8 +19,10 @@ public class OrangeLight extends LinearOpMode {
     public void runOpMode() {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(90);
-        limelight.pipelineSwitch(0);
+        limelight.pipelineSwitch(0); // ive set this pipeline to search for
         limelight.start();
+        int id = -1;
+        String motif = "null";
         waitForStart();
 
         //telemetry.addData("Id", table.getEntry("tid").getDoubleArray(new double[6]));
@@ -25,29 +30,33 @@ public class OrangeLight extends LinearOpMode {
             LLResult result = limelight.getLatestResult();
 
             if(result != null && result.isValid()) {
+                List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
                 //findID();
                 limelight.pipelineSwitch(1);
-                telemetry.addData("tag found", null);
+                for (LLResultTypes.FiducialResult fiducial : fiducials) {
+                    id = fiducial.getFiducialId();
+                }
+                telemetry.addData("tag found", id);
+                telemetry.addData("motif", motif);
+
+                switch(id) {
+                    case 21:
+                        motif = "gpp";
+                        break;
+                    case 22:
+                        motif = "pgp";
+                        break;
+                    case 23:
+                        motif = "ppg";
+                        break;
+                    default:
+                        motif = "null";
+                }
+                        telemetry.update();
             }
 
-            telemetry.update();
 
         }
     }
-/*
-    public void findID() {
 
-
-        for(int i=1; i<=3;i++){
-            limelight.pipelineSwitch(i);
-            LLResult result = limelight.getLatestResult();
-
-            if(result != null && result.isValid()) {
-                telemetry.addData("Id", i);
-                telemetry.update();
-                break;
-            }
-        }
-    }
-*/
 }
