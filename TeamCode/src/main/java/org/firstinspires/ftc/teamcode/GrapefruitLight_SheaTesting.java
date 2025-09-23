1a7841b5308ff8fe2fa2d35b6640378fcfeef7ef
SheaMcCann <154007920+SheaMcCann@users.noreply.github.com>
2025-09-23 14:50:55 -0400

    Created GrapefruitLight Class for testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GrapefruitLight_SheaTesting.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//@TeleOp(name = "grapefruitLight")
+public class GrapefruitLight_SheaTesting extends LinearOpMode {
+    Limelight3A limelight;
+
+    public void runOpMode() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100);
+        //limelight.pipelineSwitch(0);
+        limelight.start();
+        waitForStart();
+
+        //telemetry.addData("Id", table.getEntry("tid").getDoubleArray(new double[6]));
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+
+            if(result != null && result.isValid()) {
+                limelight.pipelineSwitch(1);
+                telemetry.addData("tag found", null);
+            }
+
+            telemetry.update();
+
+        }
+    }
+/*
+    public void findID() {
+
+
+        for(int i=1; i<=3;i++){
+            limelight.pipelineSwitch(i);
+            LLResult result = limelight.getLatestResult();
+
+            if(result != null && result.isValid()) {
+                telemetry.addData("Id", i);
+                telemetry.update();
+                break;
+            }
+        }
+    }
+*/
+}
