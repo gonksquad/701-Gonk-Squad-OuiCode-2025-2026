9e5d0c46c8d7394acb02a4f4905e6a5a6ea00571
Reed Kutcher <reed_kutcher@students.nksd.net>
2025-10-09 15:43:30 -0400

    Created AjarCV.java based on the sample
    Added support to detect both green and purple artifacts and expanded the ROI to the full screen.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AjarCV.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.opencv.Circle;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+
+import java.util.List;
+
+@TeleOp
+public class AjarCV extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        ColorBlobLocatorProcessor greenLocator = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(ColorRange.ARTIFACT_GREEN)
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+                .setDrawContours(true)
+                .setBoxFitColor(0)
+                .setCircleFitColor(Color.rgb(0, 255, 0))
+                .setBlurSize(5)
+                .setDilateSize(30)
+                .setErodeSize(30)
+                .setMorphOperationType(ColorBlobLocatorProcessor.MorphOperationType.CLOSING)
+                .build();
+
+        ColorBlobLocatorProcessor purpleLocator = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(ColorRange.ARTIFACT_PURPLE)
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+                .setDrawContours(true)
+                .setBoxFitColor(0)
+                .setCircleFitColor(Color.rgb(255, 0, 255))
+                .setBlurSize(5)
+                .setDilateSize(30)
+                .setErodeSize(30)
+                .setMorphOperationType(ColorBlobLocatorProcessor.MorphOperationType.CLOSING)
+                .build();
+
+        VisionPortal portal = new VisionPortal.Builder()
+                .addProcessors(greenLocator, purpleLocator)
+                .setCameraResolution(new Size(320, 240))
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .build();
+
+        telemetry.setMsTransmissionInterval(100);
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
+
+        while (opModeIsActive() || opModeInInit()) {
+            telemetry.addData("preview on/off", "... Camera Stream\n");
+
+            List<ColorBlobLocatorProcessor.Blob> purpleBlobs = purpleLocator.getBlobs();
+            List<ColorBlobLocatorProcessor.Blob> greenBlobs = greenLocator.getBlobs();
+
+            ColorBlobLocatorProcessor.Util.filterByCriteria(
+                    ColorBlobLocatorProcessor.BlobCriteria.BY_CONTOUR_AREA,
+                    50, 20000, purpleBlobs);
+            ColorBlobLocatorProcessor.Util.filterByCriteria(
+                    ColorBlobLocatorProcessor.BlobCriteria.BY_CONTOUR_AREA,
+                    50, 20000, greenBlobs);
+
+            ColorBlobLocatorProcessor.Util.filterByCriteria(
+                    ColorBlobLocatorProcessor.BlobCriteria.BY_CIRCULARITY,
+                    0.6, 1, purpleBlobs);
+            ColorBlobLocatorProcessor.Util.filterByCriteria(
+                    ColorBlobLocatorProcessor.BlobCriteria.BY_CIRCULARITY,
+                    0.6, 1, greenBlobs);
+            telemetry.addLine("Circularity Radius Center");
+
+            telemetry.update();
+            sleep(100);
+        }
+    }
+}
