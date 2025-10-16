eb32da0ec4b7cb188b7cce38d43b77d293ff6f2b
SheaMcCann <154007920+SheaMcCann@users.noreply.github.com>
2025-10-16 11:34:20 -0400

    Untested AJTest moved to this repo
    Reversed motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AJTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple; //???
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class AJTest extends LinearOpMode {
+    public DcMotor left;
+    public DcMotor right;
+
+    @Override
+    public void runOpMode() {
+
+        left = hardwareMap.get(DcMotor.class, "lm");
+        right = hardwareMap.get(DcMotor.class, "rm");
+
+
+        //left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        right.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        waitForStart();
+        while (opModeIsActive()) {
+            left.setPower(1);
+            right.setPower(1);
+        }
+    }
+}
