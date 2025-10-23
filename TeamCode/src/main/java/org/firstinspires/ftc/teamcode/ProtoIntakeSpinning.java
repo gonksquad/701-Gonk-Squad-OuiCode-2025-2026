6f7fb3d7eca5b26b43641d5b9ebd1276c5587961
Reed Kutcher <reed_kutcher@students.nksd.net>
2025-10-23 15:20:55 -0400

    Made a prototype to spin the intake
    A to intake forward, B to stop, Y to intake in reverse.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProtoIntakeSpinning.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class ProtoIntakeSpinning extends LinearOpMode {
+
+    private DcMotor intake;
+
+    @Override
+    public void runOpMode() {
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                intake.setPower(1);
+            } else if (gamepad1.y) {
+                intake.setPower(-1);
+            } else if (gamepad1.b) {
+                intake.setPower(0);
+            }
+        }
+    }
+}
