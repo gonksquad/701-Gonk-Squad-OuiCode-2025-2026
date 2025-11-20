0c1604685615813912f72be290fbe8953ce764aa
Reed Kutcher <reed_kutcher@students.nksd.net>
2025-11-20 14:37:11 -0500

    Did hardware class stuff
    I think ScrimageTeleOp.java works about the same as ScrimageBot.java but maybe a bit more organized with the hardware class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrimmageTeleOp.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "ScrimHardwareTeleOp")
+public class ScrimmageTeleOp extends LinearOpMode {
+    ScrimmageHardware hardware;
+
+    @Override
+    public void runOpMode() {
+        hardware = new ScrimmageHardware(hardwareMap);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            hardware.doIntake(gamepad2.left_trigger > 0.125, gamepad2.y);
+            hardware.doLaunch(gamepad2.right_trigger > 0.125, gamepad2.right_bumper, gamepad2.a);
+            hardware.doDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+        }
+    }
+}
