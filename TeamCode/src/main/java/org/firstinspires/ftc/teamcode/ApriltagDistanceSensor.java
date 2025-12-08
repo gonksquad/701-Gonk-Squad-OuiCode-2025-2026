fce720ebb549da53d8c7a785312662db22c52200
SheaMcCann <154007920+SheaMcCann@users.noreply.github.com>
2025-12-08 17:05:06 -0500

    Working colorSequence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ApriltagDistanceSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ApriltagDistanceSensor.java
@@ -1,48 +1,48 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-
-@TeleOp
-public class ApriltagDistanceSensor extends OpMode {
-    private Limelight3A limeLight3A;
-    private double distance;
-
-    @Override
-    public void init() {
-        limeLight3A = hardwareMap.get(Limelight3A.class, "limelight");
-        limeLight3A.pipelineSwitch(1); // AprilTag 0 pipeline
-    }
-
-    @Override
-    public void start() {
-        limeLight3A.start();
-    }
-
-    @Override
-    public void loop() {
-        // Get yaw from Control Hub IMU
-        YawPitchRollAngles orientation = bench.getOrientation();
-        limeLight3A.updateRobotOrientation(orientation.getYaw(AngleUnit.DEGREES));
-
-        // Get latest Limelight result, pipeline 8 for AprilTag 0
-        LLResult llResult = limeLight3A.getLatestResult();
-        if (llResult != null && llResult.isValid()) {
-            Pose3D botpose = llResult.getBotpose_MT2();
-            telemetry.addData("Calculated Distance", distance);
-            telemetry.addData("Target X", llResult.getTx());
-            telemetry.addData("Target Area", llResult.getTa());
-            telemetry.addData("Botpose", botpose.toString());
-        } else {
-            telemetry.addData("AprilTag", "Not Found");
-        }
-        telemetry.update();
-    }
-
-}
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.hardware.limelightvision.LLResult;
+//import com.qualcomm.hardware.limelightvision.Limelight3A;
+//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+//import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+//import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+//
+//@TeleOp
+//public class ApriltagDistanceSensor extends OpMode {
+//    private Limelight3A limeLight3A;
+//    private double distance;
+//
+//    @Override
+//    public void init() {
+//        limeLight3A = hardwareMap.get(Limelight3A.class, "limelight");
+//        limeLight3A.pipelineSwitch(1); // AprilTag 0 pipeline
+//    }
+//
+//    @Override
+//    public void start() {
+//        limeLight3A.start();
+//    }
+//
+//    @Override
+//    public void loop() {
+//        // Get yaw from Control Hub IMU
+//        YawPitchRollAngles orientation = bench.getOrientation();
+//        limeLight3A.updateRobotOrientation(orientation.getYaw(AngleUnit.DEGREES));
+//
+//        // Get latest Limelight result, pipeline 8 for AprilTag 0
+//        LLResult llResult = limeLight3A.getLatestResult();
+//        if (llResult != null && llResult.isValid()) {
+//            Pose3D botpose = llResult.getBotpose_MT2();
+//            telemetry.addData("Calculated Distance", distance);
+//            telemetry.addData("Target X", llResult.getTx());
+//            telemetry.addData("Target Area", llResult.getTa());
+//            telemetry.addData("Botpose", botpose.toString());
+//        } else {
+//            telemetry.addData("AprilTag", "Not Found");
+//        }
+//        telemetry.update();
+//    }
+//
+//}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSequence.java
@@ -8,6 +8,8 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.List;
+
 @TeleOp
 public class colorSequence extends LinearOpMode{
     Limelight3A limelight;
@@ -15,29 +17,85 @@ public class colorSequence extends LinearOpMode{
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         waitForStart();
         limelight.start();
-        telemetry.addLine("runOpMode started");
         while (opModeIsActive()) {
-            telemetry.addLine("opModeIsActive started");
-            limelight.pipelineSwitch(2);
+            //telemetry.addLine("opModeIsActive started");
+            limelight.pipelineSwitch(1);
+            telemetry.update();
+
+            int id = -1;
+            String motif = "null";
             LLResult result = limelight.getLatestResult();
-            if (result != null && result.isValid()) {
-                telemetry.addLine("pgp");
-            }
-            else {
-                limelight.pipelineSwitch(3);
-                if (result != null && result.isValid()) {
-                    telemetry.addLine("gpp");
+            //BoundingBox();
+            if(result != null && result.isValid()) {
+                List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
+                //findID();
+                for (LLResultTypes.FiducialResult fiducial : fiducials) {
+                    id = fiducial.getFiducialId();
                 }
-                else{
-                    limelight.pipelineSwitch(4);
-                    if (result != null && result.isValid()) {
-                        telemetry.addLine("ppg");
-                    }
-                    else{
-                        telemetry.addLine("No apriltag detected.");
-                    }
+
+                switch(id) {
+                    case 21:
+                        motif = "gpp";
+                        break;
+                    case 22:
+                        motif = "pgp";
+                        break;
+                    case 23:
+                        motif = "ppg";
+                        break;
+                    default:
+                        motif = "null";
                 }
+
+                telemetry.addData("tag found", id);
+                telemetry.addData("motif", motif);
+                telemetry.update();
             }
         }
     }
 }
+
+
+//package org.firstinspires.ftc.teamcode;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.hardware.limelightvision.LLResult;
+//import com.qualcomm.hardware.limelightvision.LLResultTypes;
+//import com.qualcomm.hardware.limelightvision.LLStatus;
+//import com.qualcomm.hardware.limelightvision.Limelight3A;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//@TeleOp
+//public class colorSequence extends LinearOpMode{
+//    Limelight3A limelight;
+//    public void runOpMode(){
+//        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+//        waitForStart();
+//        limelight.start();
+//        telemetry.addLine("runOpMode started");
+//        while (opModeIsActive()) {
+//            telemetry.addLine("opModeIsActive started");
+//            limelight.pipelineSwitch(2);
+//            LLResult result = limelight.getLatestResult();
+//            if (result != null && result.isValid()) {
+//                telemetry.addLine("pgp");
+//            }
+//            else {
+//                limelight.pipelineSwitch(3);
+//                if (result != null && result.isValid()) {
+//                    telemetry.addLine("gpp");
+//                }
+//                else{
+//                    limelight.pipelineSwitch(4);
+//                    if (result != null && result.isValid()) {
+//                        telemetry.addLine("ppg");
+//                    }
+//                    else{
+//                        telemetry.addLine("No apriltag detected.");
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
