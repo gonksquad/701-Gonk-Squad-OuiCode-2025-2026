17084d5172bbcf987ac0fbecfbdabbef99f2cebe
SheaMcCann <154007920+SheaMcCann@users.noreply.github.com>
2025-12-09 20:02:50 -0500

    rahh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AwesomeSheaLimelightDistance/blueGoalTargeting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AwesomeSheaLimelightDistance/blueGoalTargeting.java
@@ -9,36 +9,41 @@ import com.qualcomm.robotcore.hardware.Servo;
 import java.util.List;
 
 @TeleOp
-public class NewestATDistance extends LinearOpMode{
+public class blueGoalTargeting extends LinearOpMode{
     Limelight3A limelight;
     Servo servo;
+    Servo fakeTurret;
     double servoPosition;
+
     public void runOpMode(){
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         servo = hardwareMap.get(Servo.class, "servo");
+        fakeTurret = hardwareMap.get(Servo.class, "fakeTurret");
+        limelight.pipelineSwitch(6);
+
         waitForStart();
         limelight.start();
-        limelight.pipelineSwitch(1);
         servo.setPosition(0.5);
+        servo.setPosition(0.0);
 
-        while (opModeIsActive()) {
+        while (opModeIsActive()){
             LLResult result = limelight.getLatestResult();
             double id = -1;
-            if(result != null && result.isValid()) {
-                List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
 
+            if (result != null && result.isValid()){
+                List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
                 for (LLResultTypes.FiducialResult fiducial : fiducials) {
-                    id = fiducial.getTargetXDegrees();
+                    id = fiducial.getFiducialId();
                 }
 
-                telemetry.addData("Tag Found, X Degrees:", id);
+                telemetry.addLine("Blue Goal Found.");
                 telemetry.update();
 
                 if (result.getTx() >5){
                     while (result.getTx() >5){
-                        servoPosition = servo.getPosition() + 0.02;
+                        servoPosition = servo.getPosition() + 0.01;
                         if (servoPosition < 0){
-                            servoPosition = 0.0;
+                            servoPosition = 0.0
                         }
                         servo.setPosition(servoPosition);
                         result = limelight.getLatestResult();
@@ -48,7 +53,7 @@ public class NewestATDistance extends LinearOpMode{
                     }
                 } else if (result.getTx() < -5){
                     while (result.getTx() <-5){
-                        servoPosition = servo.getPosition() - 0.02;
+                        servoPosition = servo.getPosition() - 0.01;
                         if (servoPosition > 1){
                             servoPosition = 1.0;
                         }
@@ -60,13 +65,7 @@ public class NewestATDistance extends LinearOpMode{
                     }
                 }
                 telemetry.update();
-
-            } else {
-                telemetry.addLine("No apriltag found");
-                telemetry.update();
-                //servo.setPosition(0.5);
             }
         }
     }
 }
-
