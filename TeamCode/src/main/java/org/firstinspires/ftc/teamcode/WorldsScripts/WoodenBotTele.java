de89e1562cde6b6ad6793a46fb4741e47d806789
danny-reilly <daniel_reilly@students.nksd.net>
2026-03-09 15:16:30 -0400

    tele changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsScripts/WoodenBotTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsScripts/WoodenBotTele.java
@@ -11,11 +11,11 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class WoodenBotTele extends LinearOpMode {
 
     DcMotor driveFr, driveFl, driveBr, driveBl;
-    DcMotor intakeMotor;
+    DcMotor intakeMotorLeft, intakeMotorRight;
     DcMotorEx outtakeMotorR, outtakeMotorL;
     Servo blocker;
-    boolean isBlocking = false;
-    boolean intakeBtn, flushBtn, blockBtn;
+    //boolean isBlocking = false;
+    boolean intakeBtn, flushBtn, blockBtn, longRangeBtn;
 
 
     public void runOpMode() {
@@ -33,8 +33,10 @@ public class WoodenBotTele extends LinearOpMode {
         driveBl = hardwareMap.get(DcMotor.class, "bl");
             driveBl.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        intakeMotor = hardwareMap.get(DcMotor.class, "intakel");
-            intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        intakeMotorLeft = hardwareMap.get(DcMotor.class, "intakel");
+            intakeMotorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        intakeMotorRight = hardwareMap.get(DcMotor.class, "intaker");
+            intakeMotorRight.setDirection(DcMotorSimple.Direction.FORWARD);
         blocker = hardwareMap.get(Servo.class, "blocker");
 
         outtakeMotorR = hardwareMap.get(DcMotorEx.class, "outr");
@@ -43,26 +45,35 @@ public class WoodenBotTele extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
-            intakeBtn = gamepad2.x;
+            blockBtn = gamepad2.a;
+            intakeBtn = gamepad2.x || blockBtn;
             flushBtn = gamepad2.b;
-            blockBtn = gamepad2.aWasPressed();
+            longRangeBtn = gamepad2.right_trigger>0.05;
             SpinIntake(1); // x for intake, a for flush
-            ToggleOuttaking(1, 0, 1200);
+            ToggleOuttaking();
             SetMotorPowers(1);
+            telemetry.update();
         }
     }
 
     public void SpinIntake(float intakeSpeed) {
-        intakeMotor.setPower(intakeBtn ? intakeSpeed : flushBtn ? -intakeSpeed : 0);
+        intakeMotorLeft.setPower(intakeBtn ? intakeSpeed : flushBtn ? -intakeSpeed : 0);
+        intakeMotorRight.setPower(intakeBtn ? intakeSpeed : flushBtn ? -intakeSpeed : 0);
+
     }
-    public void ToggleOuttaking(float open, float blocked, float outtakeVelocitas) {
-        isBlocking = blockBtn ? !isBlocking : isBlocking;
-        blocker.setPosition(isBlocking ? blocked : (outtakeMotorR.getVelocity() >=  outtakeVelocitas) ? open : blocked);
+    public void ToggleOuttaking() {
+        final int outtakeVelocityShort = 1200;
+        final int outtakeVelocityLong = 1400;
+        final int outtakeVelocityIdle = 1050;
+        blocker.setPosition(blockBtn ? 0 : 1);
+        telemetry.addData("speedR", outtakeMotorR.getVelocity());
+        telemetry.addData("speedL", outtakeMotorL.getVelocity());
         // sets the blocker position to open once motors are at certain speed (wont work but we'll have this evench)
         //sleep(100000000);
-        outtakeMotorR.setVelocity(isBlocking ? outtakeVelocitas : 0);
-        outtakeMotorL.setVelocity(isBlocking ? outtakeVelocitas : 0);
-
+        //outtakeMotorR.setVelocity(blockBtn ? outtakeVelocityShort : outtakeVelocityShort);
+        //outtakeMotorL.setVelocity(blockBtn ? outtakeVelocityShort : outtakeVelocityShort);
+        outtakeMotorR.setVelocity(blockBtn ? outtakeVelocityShort : outtakeVelocityIdle);
+        outtakeMotorL.setVelocity(blockBtn ? (!longRangeBtn ? outtakeVelocityShort : outtakeVelocityLong) : outtakeVelocityIdle);
     }
     public void SetMotorPowers(float maxSpeed) {
         double y = -gamepad1.left_stick_y*maxSpeed;
