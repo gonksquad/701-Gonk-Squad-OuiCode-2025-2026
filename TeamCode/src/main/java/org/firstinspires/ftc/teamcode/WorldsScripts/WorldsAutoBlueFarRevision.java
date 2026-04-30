c96847b8931572f7e3e3050cd1ae817d3d3c5cb1
Vienna <stardog727@gmail.com>
2026-04-30 18:14:15 -0400

    vienna and shea making mirror red to blue far auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsScripts/WorldsAutoBlueFarRevision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WorldsScripts/WorldsAutoBlueFarRevision.java
@@ -15,10 +15,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Autonomous
-public class WorldsAutoBlueFar extends LinearOpMode {
+public class WorldsAutoBlueFarRevision extends LinearOpMode {
     MecanumDrive drive;
     WorldsAutoHardware hardware;
-    Pose2d launchPos = new Pose2d(61, -15, Math.toRadians(-180)); //angle negative?? bad maybe :(
+    Pose2d launchPos = new Pose2d(61, -15, Math.toRadians(-180));
     Pose2d launchPos2 = new Pose2d(55, -10, Math.toRadians(-135));
 
     @Override
@@ -26,84 +26,56 @@ public class WorldsAutoBlueFar extends LinearOpMode {
         drive = new MecanumDrive(hardwareMap, launchPos);
         hardware = new WorldsAutoHardware(hardwareMap);
         Action pickup3 = drive.actionBuilder(launchPos)
-                .setTangent(Math.toRadians(210)) //angle not changed here
-                .splineToSplineHeading(new Pose2d(23, -43, Math.toRadians(-90)), Math.toRadians(90))
+                .setTangent(Math.toRadians(-210))
+                .splineToSplineHeading(new Pose2d(36, -43, Math.toRadians(-90)), Math.toRadians(-90))
                 .lineToY(-63)
                 .setReversed(true)
-                .splineToLinearHeading(launchPos2, Math.toRadians(45)) //not neg
+                .splineToLinearHeading(launchPos2, Math.toRadians(-45))
                 .build();
 
 
         Action flushPickup = drive.actionBuilder(launchPos2)
                 .setReversed(false)
-                .setTangent(Math.toRadians(90))
-                .strafeToLinearHeading(new Vector2d(launchPos.position.x-2,-56), Math.toRadians(-75))
+                .setTangent(Math.toRadians(-90))
+                .strafeToLinearHeading(new Vector2d(launchPos.position.x-2,-56), Math.toRadians(75))
                 .waitSeconds(0.2)
                 .lineToY(-52)
                 .lineToY(-58)
                 .strafeToLinearHeading(launchPos2.position, launchPos2.heading)
                 .build();
-
-        Action flushPickup2 =  drive.actionBuilder(launchPos2)
-                .setReversed(false)
-                .setTangent(Math.toRadians(90))
-                .strafeToLinearHeading(new Vector2d(launchPos.position.x-2,-56), Math.toRadians(-75))
-                .waitSeconds(0.2)
-                .lineToY(-52)
-                .lineToY(-58)
-                .strafeToLinearHeading(launchPos2.position, launchPos2.heading)
-                .build();
-        Action flushPickup3 = drive.actionBuilder(launchPos2)
-                .setReversed(false)
-                .setTangent(Math.toRadians(90))
-                .strafeToLinearHeading(new Vector2d(launchPos.position.x-2,-56), Math.toRadians(-75))
-                .waitSeconds(0.2)
-                .lineToY(-52)
-                .lineToY(-58)
-                .strafeToLinearHeading(launchPos2.position, launchPos2.heading)
-                .build();
-        Action flushPickup4 = drive.actionBuilder(launchPos2)
-                .setReversed(false)
-                .setTangent(Math.toRadians(90))
-                .strafeToLinearHeading(new Vector2d(launchPos.position.x-2,-56), Math.toRadians(-75))
-                .waitSeconds(0.2)
-                .lineToY(-52)
-                .lineToY(-58)
-                .strafeToLinearHeading(launchPos2.position, launchPos2.heading)
-                .build();
-
 
         Action endPark = drive.actionBuilder(launchPos2)
                 .strafeToLinearHeading(new Vector2d(41, -34), Math.toRadians(180))
                 .build();
 
-//more stuff
+
         waitForStart();
 
         Actions.runBlocking(
                 new SequentialAction(
                         new ParallelAction(
                             hardware.blockOuttake(),
-                            hardware.setYawAngle(20),
-                            hardware.setOuttakeVelStart(50), //so much lower
-                            hardware.intakeStart()
+                            hardware.setYawAngle(-20),
+                            hardware.setOuttakeVelStart(1550),
+                                hardware.intakeStart()
                         ),
-                        hardware.blockOuttake(),
+                        new SleepAction(1),
+                        hardware.unblockOuttake(),
+                        hardware.setOuttakeVelStart(1600),
                         //Turn turret, shoot 3, turn on intake, block turret
-                        hardware.setHoodPos(0.3),
-                        hardware.launch(1, 0.65, 0.7, 80),
+                        hardware.setHoodPos(0.5),
                         new SleepAction(1.5),
                         hardware.blockOuttake(),
                         new ParallelAction(
-                            hardware.setYawAngle(-25),
+                            hardware.setYawAngle(15),
                             pickup3
                         ),
                         hardware.setHoodPos(0.4),
-                        hardware.launch(1600, 0.3, 0.1, 400),
+                        hardware.launch(1500, 0.3, 0.1, 400),
                         new SleepAction(1.5),
                         hardware.blockOuttake(),
                         //turn turret, shoot 3, block turret
-                        hardware.setYawAngle(-10),
+                        hardware.setYawAngle(21),
                         flushPickup,
                         hardware.setHoodPos(0.4),
                         hardware.launch(1600, 0.4, 0.25, 500),
@@ -113,27 +85,27 @@ public class WorldsAutoBlueFar extends LinearOpMode {
 
                         hardware.blockOuttake(),
                         //turn turret, shoot 3, block turret
-                        hardware.setYawAngle(-10),
-                        flushPickup2,
+                        hardware.setYawAngle(21),
+                        flushPickup,
                         hardware.setHoodPos(0.4),
-                        hardware.launch(1400, 0.4, 0.25, 500),
+                        hardware.launch(1600, 0.4, 0.25, 500),
                         new SleepAction(1.5),
                         //shoot 3, block turret
 
                         hardware.blockOuttake(),
                         //turn turret, shoot 3, block turret
-                        hardware.setYawAngle(-10), //26
-                        flushPickup3,
+                        hardware.setYawAngle(21),
+                        flushPickup,
                         hardware.setHoodPos(0.4),
-                        hardware.launch(1400, 0.4, 0.25, 500),
+                        hardware.launch(1600, 0.4, 0.25, 500),
                         new SleepAction(1.5),
 
                         hardware.blockOuttake(),
                         //turn turret, shoot 3, block turret
-                        hardware.setYawAngle(-10),
-                        flushPickup4,
+                        hardware.setYawAngle(21),
+                        flushPickup,
                         hardware.setHoodPos(0.4),
-                        hardware.launch(1400, 0.4, 0.25, 500),
+                        hardware.launch(1600, 0.4, 0.25, 500),
                         new SleepAction(1.5),
 
                         endPark
